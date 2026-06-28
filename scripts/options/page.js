53cde873460d57f85d79eaa48871469cea5ba777
Alexander Popov <alex.wayfer@gmail.com>
2026-06-28 11:01:16 +0300

    Fix error in Options page script

--- scripts/options/page.js
+++ scripts/options/page.js
@@ -2,7 +2,7 @@ import { loadOptions } from '../load-options.js'
 import { LabelsForm } from './labels-form.js'
 
 document.addEventListener('DOMContentLoaded', async _event => {
-	const options loadOptions()
+	const options = await loadOptions()
 
 	const labelsForm = new LabelsForm(document.querySelector('form[name="labels"]'), options)
 })
